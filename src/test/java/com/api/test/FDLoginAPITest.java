ca5af39fbc572c42e86652d69c7580a3e9a167fa
Abhilasha <abhilashag24@gmail.com>
2026-09-10 12:17:54 +0400

    Created ConfigManager class for managing the multiple environments (dev,qa,uat)for the project.
    Updated pom.xml file -> upgraded maven-compiler source and target from
    11 to 14.

--- src/test/java/com/api/test/FDLoginAPITest.java
+++ src/test/java/com/api/test/FDLoginAPITest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
 import com.api.pojo.UserCredentials;
+import static com.api.utils.ConfigManager.*;
 
 import io.restassured.http.ContentType;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
@@ -16,10 +17,11 @@ public class FDLoginAPITest {
 	@Test
 	
 	public void loginAPITest() {
+			
 		UserCredentials userCredentials =  new UserCredentials("iamfd", "password");
 		
 		given()
-			.baseUri("http://64.227.160.186:9000/v1")
+			.baseUri(getProperty("BASE_URI"))
 			.and()
 			.contentType(ContentType.JSON)
 			.and()
---
+++ src/test/java/com/api/utils/ConfigManager.java
@@ -0,0 +1,55 @@
+package com.api.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+
+	// Write a program to read properties file from
+	// src/test/resources/config/config.properties
+
+	private static String path = "config/config.properties";
+	private static Properties properties = new Properties();
+	private static String env;
+
+	static {
+		// If no evn is mentioned , then assign qa hence the second parameter
+		env = System.getProperty("env", "qa");
+		switch (env.toLowerCase().trim()) {
+		case "dev" -> path = "config/config.dev.properties";
+
+		case "qa" -> path = "config/config.qa.properties";
+
+		case "uat" -> path = "config/config.uat.properties";
+
+		default -> path = "config/config.qa.properties";
+		
+		}
+		// Operation of loading a property file in the memory
+		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+
+		if (path == null) {
+			throw new RuntimeException("Not able to find properties file at given path " + path);
+		}
+		try {
+			properties.load(input);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	// To restrict any class to create an object of ConfigManager
+	private ConfigManager() {
+
+	}
+
+	public static String getProperty(String key) {
+
+		return properties.getProperty(key);
+
+	}
+
+}
