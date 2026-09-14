f1f10335fd721bc22b31adae03d20384d5e3daa1
Abhilasha <abhilashag24@gmail.com>
2026-09-14 10:13:40 +0400

    DateTimeUtility
    Created Date Time Utiity class to provide past ISO Standard UTC Time

--- src/test/java/com/api/test/CreateJobAPITest.java
+++ src/test/java/com/api/test/CreateJobAPITest.java
@@ -1,6 +1,7 @@
 package com.api.test;
 
 import static com.api.constants.Role.FD;
+import static com.api.utils.DateTimeUtility.getTimeWithDaysAgo;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
@@ -26,21 +27,21 @@ public class CreateJobAPITest {
 		Customer customer = new Customer("Test_FN", "Test_LN", "9856321452", "", "test@test.com", "");
 		CustomerAddress customerAddress = new CustomerAddress("101", "Test Apartment", "Test Street", "Inorbit mall",
 				"Test Area", "451245", "India", "Maharashtra");
-		CustomerProduct customerProduct = new CustomerProduct("2026-04-30T20:00:00.000Z", "73058861592677",
+		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "73058861592677",
 				"73058861592677", "73058861592677", "2026-04-30T20:00:00.000Z", "1", "1");
 
 		Problems problems = new Problems(1, "Battery Issue");
-		List<Problems> problemsList = new ArrayList<Problems>();		
+		List<Problems> problemsList = new ArrayList<Problems>();
 		problemsList.add(problems);
 
 		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct,
 				problemsList);
 
 		given().spec(SpecUtil.requestSpecWithAuthToken(FD, createJobPayload)).when().post("/job/create").then()
-				.spec(SpecUtil.responseSpec_OK()).body(matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
-				.body("message", equalTo("Job created successfully. ")).and().body("data.mst_service_location_id", equalTo(1))
-				.and().body("data.job_number", startsWith("JOB_"));
-		
+				.spec(SpecUtil.responseSpec_OK())
+				.body(matchesJsonSchemaInClasspath("response-schema/CreateJobAPIResponseSchema.json"))
+				.body("message", equalTo("Job created successfully. ")).and()
+				.body("data.mst_service_location_id", equalTo(1)).and().body("data.job_number", startsWith("JOB_"));
 
 	}
 
