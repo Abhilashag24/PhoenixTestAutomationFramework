2aa2901368e88d9759ae6d1d0e59968b665da7c3
Abhilasha <abhilashag24@gmail.com>
2026-09-17 09:51:33 +0400

    Created Faker demo for practice purpose

---
+++ src/test/java/com/api/utils/FakerDemo.java
@@ -0,0 +1,21 @@
+package com.api.utils;
+
+import java.util.Locale;
+
+import com.github.javafaker.Faker;
+
+public class FakerDemo {
+	public static void main(String[] args) {
+
+		Faker faker = new Faker(new Locale("en-IND"));
+		System.out.println(faker.name().fullName());
+		System.out.println(faker.address().country());
+		System.out.println(faker.address().state());
+		System.out.println(faker.address().cityName());
+		System.out.println(faker.relationships().sibling());
+		System.out.println(faker.numerify("870#######"));
+		System.out.println(faker.numerify("870#######"));
+		System.out.println(faker.numerify("870#######"));
+	}
+
+}
