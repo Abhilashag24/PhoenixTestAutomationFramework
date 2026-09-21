7dbaab2b61a7cc4ab51a4aae6ba1a1da1fee4890
Abhilasha <abhilashag24@gmail.com>
2026-09-21 17:49:42 +0400

    Updated the DataBaseManager to use HikariCP Library

---
+++ src/test/java/com/database/DemoRunner.java
@@ -0,0 +1,15 @@
+package com.database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DemoRunner {
+
+	public static void main(String[] args) throws SQLException {
+		Connection con = DataBaseManager.getConnection();
+		System.out.println(con);
+		
+
+	}
+
+}
